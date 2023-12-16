94afe3e69a95259b41151f5ae0e5511c60cf2547
DmytroAntonovych703 <edward1997703@gmail.com>
2023-12-16 05:18:12 -0800

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Threejs-3D-Portfolio"
+  base: "/3D-portfolio"
 })

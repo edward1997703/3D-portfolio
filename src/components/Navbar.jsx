e35e3eabb61762d4bc8249b6fbbc17fc4195ae5e
DmytroAntonovych703 <edward1997703@gmail.com>
2023-12-29 02:59:29 -0900

    modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-          Serhii Andros
+          Dmytro Antonovych
           </p>
         </Link>
 
